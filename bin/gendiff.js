9731540a142c6d6465a6ae9f7208c7609905b5f9
nikitazh12 <nikitazh12@gmail.com>
2024-04-20 12:24:32 +0300

    done 2nd step

---
+++ bin/gendiff.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import process from 'process';
+
+const program = new Command();
+
+program
+	.name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+	.arguments('<filepath1>  <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  
+	
+program.parse(process.argv);
